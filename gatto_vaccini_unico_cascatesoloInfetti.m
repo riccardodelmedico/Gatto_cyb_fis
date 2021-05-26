6c6a83828c4b018a5ca1cea0603ccbd4651f3aa7
giovanni-di-donato <84123970+giovanni-di-donato@users.noreply.github.com>
2021-05-26 10:49:07 +0200

    Add files via upload

---
+++ gatto_vaccini_unico_cascatesoloInfetti.m
@@ -0,0 +1,87 @@
+function xdot= gatto_vaccini_unico_cascatesoloInfetti(t,x)
+
+global lambda deltaE deltaP sig eta gammaI alfaI gammaA zeta gammaH alfaH ...
+    gammaQ betaP betaA betaI N x0 eff1 eff2 ef1 prima_d seconda_d ...
+    Lvect teta
+
+S = x(1); %suscettibili
+E = x(2); %esposti
+P = x(3); %presintomatici
+I1 = x(4);%infetti
+I2 = x(5);
+I3 = x(6);
+A = x(7); %asintomatici
+H = x(8); %ospedalizzati
+Q = x(9); %quarantena casalinga
+R = x(10); %recuperati
+D = x(11); %dead
+% parameters_vaccini;
+% dati_vaccini;
+S1 = x(12); %suscettibili
+E1 = x(13); %esposti
+P1 = x(14); %presintomatici
+I11 = x(15);%infetti
+I12 = x(16);
+I13 = x(17);
+A1 = x(18); %asintomatici
+H1 = x(19); %ospedalizzati
+Q1 = x(20); %quarantena casalinga
+R1 = x(21); %recuperati
+D1 = x(22); %dead
+% parameters_vaccini;
+% dati_vaccini;
+S2 = x(23); %suscettibili
+E2 = x(24); %esposti gruppo
+P2 = x(25); %presintomatici
+I21 = x(26);%infetti
+I22 = x(27);
+I23 = x(28);
+A2 = x(29); %asintomatici
+R2 = x(30); %recuperati
+
+prima_dos=prima_d(fix(t)+1);
+seconda_dos=seconda_d(fix(t)+1);
+L=Lvect(fix(t)+1);
+
+
+lambda= (betaP*(P+P1+P2) + betaI*(I3+I13+I23) + betaA*(A+A1+A2))/((S+S1+S2) + (E+E1+E2) + (I3+I13+I23) + (A+A1+A2) + (R+R1+R2));
+xdot = zeros(30,1);
+
+    xdot(1) = - lambda*S*(1-teta*L)^2 - eff1*prima_dos;
+    xdot(2) = lambda*S*(1-teta*L)^2 - deltaE*E;
+    xdot(3) = deltaE*E - deltaP*P;
+    xdot(4) = sig*deltaP*P - (eta + gammaI + alfaI)*3*I1;
+    xdot(5) = (eta + gammaI + alfaI)*3*I1 - (eta + gammaI + alfaI)*3*I2;
+    xdot(6) = (eta + gammaI + alfaI)*3*I2 - (eta + gammaI + alfaI)*3*I3;
+    xdot(7) = (1 - sig)*deltaP*P - gammaA*A;
+    xdot(8) = (1 - zeta)*eta*I3 - (gammaH + alfaH)*H;
+    xdot(9) = zeta*eta*I3 - gammaQ*Q;
+    xdot(10) = gammaI*I3 + gammaA*A + gammaH*H;
+    xdot(11) = alfaI*I3 + alfaH*H;
+    %%%%POI LA SECONDA DINAMICA, CHE è ACCOPPIATA TRAMITE LAMBDA
+    
+    xdot(12) = - lambda*S1*(1-teta*L)^2+eff1*prima_dos-eff2*seconda_dos;
+    xdot(13) = lambda*S1*(1-teta*L)^2 - deltaE*E1;
+    xdot(14) = deltaE*E1 - deltaP*P1;
+    xdot(15) = sig*deltaP*P1 - ((1-ef1)*eta + gammaI +(1-ef1)*alfaI)*3*I1;
+    xdot(16) = ((1-ef1)*eta + gammaI +(1-ef1)*alfaI)*3*I11 - ((1-ef1)*eta + gammaI +(1-ef1)*alfaI)*3*I12;
+    xdot(17) = ((1-ef1)*eta + gammaI +(1-ef1)*alfaI)*3*I12 - ((1-ef1)*eta + gammaI +(1-ef1)*alfaI)*3*I13;
+    xdot(18) = (1 - sig)*deltaP*P1 - gammaA*A1;
+    xdot(19) = (1 - zeta)*(1-ef1)*eta*I13 - (gammaH +(1-ef1)*alfaH)*H1;
+    xdot(20) = zeta*(1-ef1)*eta*I13 - gammaQ*Q1;
+    xdot(21) = gammaI*I13 + gammaA*A1 + gammaH*H1;
+    xdot(22) = (1-ef1)*alfaI*I13 + (1-ef1)*alfaH*H1;
+    
+    %%%%%E SIMILMENTE LA TERZA
+    xdot(23) = - lambda*S2*(1-teta*L)^2+eff2*seconda_dos;
+    xdot(24) = lambda*S2*(1-teta*L)^2 - deltaE*E2;
+    xdot(25) = deltaE*E2 - deltaP*P2;
+    xdot(26) = sig*deltaP*P2 - gammaA*3*I21;
+    xdot(27) = gammaA*3*I21 - gammaA*3*I22;
+    xdot(28) = gammaA*3*I22 - gammaA*3*I23;
+    xdot(29) = (1 - sig)*deltaP*P2 - gammaA*A2;
+    xdot(30) = gammaA*I23 + gammaA*A2;
+
+
+end
+
--- main.m
+++ main.m
@@ -22,13 +22,17 @@ novax= 62;
 N= nolockdown + novax + NV;
 time= 0:1:N-1;
 E0= 100/pop;
-x0= [1-E0 E0 zeros(1,22)]; 
+ 
 prima_d= [zeros(novax+nolockdown,1); prima_dose_norm];
 seconda_d=[zeros(novax+nolockdown,1); seconda_dose_norm];
+%%
+figure(1)
+plot(time, prima_d)
+hold on
+plot(time,seconda_d)
+hold off
 
-
-
-
+%
 % %% Caricamento dati iniziali opzione 2
 % %si utilizzano i valori reali dell'epidemia nel giorno in cui sono
 % %cominciate le vaccinazioni (27/12/2020) (dati da
@@ -47,10 +51,10 @@ seconda_d=[zeros(novax+nolockdown,1); seconda_dose_norm];
 
 %%
 %VEDIAMO CHE SUCCEDE ACCOPPIANDO LE DINAMICHE
-Lvect= [zeros(nolockdown,1); 0.7.*ones(novax,1); 0.45*ones(136,1)]; %si suppone che il lockdown
+Lvect= [zeros(nolockdown,1); 0.5*ones(novax,1); 0.5*ones(136,1)]; %si suppone che il lockdown
 %duri a partire dal 18esimo giorno e termini il giorno in cui cominciano le
 %vaccinazioni
-
+x0= [1-1*E0 1*E0 zeros(1,22)];
 
 tic
 [t,x_vaccini_tot]= ode45('gatto_vaccini_unico', time, x0); 
@@ -76,9 +80,9 @@ plot(t, x_vaccini_tot(:,19:24))
 legend('S2(t)','E2(t)', 'P2(t)', 'I2(t)', 'A2(t)','R2(t)')
 %
 
-%% ora introduciamo le cascate di Ode
+%% ora introduciamo le cascate di Ode ma secondo il Gatto (su E e su H come a pgina 11)
 global x0_casc
-Lvect= [zeros(nolockdown,1); 0.3*ones(novax,1); 0.2*ones(136,1)]; %si suppone che il lockdown
+Lvect= [zeros(nolockdown,1); 0.3*ones(novax,1); 0.3*ones(136,1)]; %si suppone che il lockdown
 %duri a partire dal 18esimo giorno e termini il giorno in cui cominciano le
 %vaccinazioni
 x0_casc = [1-1*E0 , 1*E0, zeros(1,29)];
@@ -104,4 +108,77 @@ legend('E1(t)','E2(t)', 'P(t)', 'I(t)', 'A(t)', 'H1(t)','H2(t)','H3(t)', 'Q(t)',
 figure(5) %altre variabili 3 gatto
 plot(t, x_vaccini_tot(:,26:31))
 legend('E21(t)','E22(t)', 'P2(t)', 'I2(t)', 'A2(t)','R2(t)')
+
+%% ora facciamo la cascata di 3 ode su infetti come suggerito da manfredi
+
+global x0_casc_inf
+Lvect= [zeros(nolockdown,1); 0.6*ones(novax,1); 0.5*ones(136,1)]; %si suppone che il lockdown
+%duri a partire dal 18esimo giorno e termini il giorno in cui cominciano le
+%vaccinazioni
+x0_casc_inf = [1-1*E0 , 1*E0, zeros(1,28)];
+tic
+[t,x_vaccini_tot]= ode45('gatto_vaccini_unico_cascatesoloInfetti', time, x0_casc_inf); 
+toc
+
+figure(1)
+plot(t, x_vaccini_tot(:,:)) %plot totale
+
+figure(2)
+plot(t, x_vaccini_tot(:,[1 12 23])) %visione suscettibili
+legend('S(t)','S1(t)','S2(t)')
+
+figure(3)
+plot(t, x_vaccini_tot(:,[2,3, 6:8])) %altre variabili 1 gatto
+legend('E(t)','P(t)','I3(t)', 'A(t)', 'H(t)', 'Q(t)', 'R(t)', 'D(t)')
+
+figure(4) %altre variabili 2 gatto
+plot(t, x_vaccini_tot(:,13:22))
+legend('E1(t)','P1(t)', 'I13(t)', 'A1(t)', 'H1(t)', 'Q1(t)', 'R1(t)', 'D1(t)')
+
+figure(5) %altre variabili 3 gatto
+plot(t, x_vaccini_tot(:,23:30))
+legend('E2(t)','P2(t)', 'I12(t)','I22(t)','I23(t)', 'A2(t)', 'R2(t)')
+
+%%
+x = x_vaccini_tot;
+
+
+S = x(:,1); %suscettibili
+E = x(:,2); %esposti
+P = x(:,3); %presintomatici
+I1 = x(:,4);%infetti
+I2 = x(:,5);
+I3 = x(:,6);
+A = x(:,7); %asintomatici
+H = x(:,8); %ospedalizzati
+Q = x(:,9); %quarantena casalinga
+R = x(:,10); %recuperati
+D = x(:,11); %dead
+% parameters_vaccini;
+% dati_vaccini;
+S1 = x(:,12); %suscettibili
+E1 = x(:,13); %esposti
+P1 = x(:,14); %presintomatici
+I11 = x(:,15);%infetti
+I12 = x(:,16);
+I13 = x(:,17);
+A1 = x(:,18); %asintomatici
+H1 = x(:,19); %ospedalizzati
+Q1 = x(:,20); %quarantena casalinga
+R1 = x(:,21); %recuperati
+D1 = x(:,22); %dead
+% parameters_vaccini;
+% dati_vaccini;
+S2 = x(:,23); %suscettibili
+E2 = x(:,24); %esposti gruppo
+P2 = x(:,25); %presintomatici
+I21 = x(:,26);%infetti
+I22 = x(:,27);
+I23 = x(:,28);
+A2 = x(:,29); %asintomatici
+R2 = x(:,30); 
+
+lambda= (betaP*(P+P1+P2) + betaI*(I3+I13+I23) + betaA*(A+A1+A2))./(S + E + (I3+I13+I23) + (A+A1+A2) + R);
+plot(time,lambda)
 %
+plot(time,E)
