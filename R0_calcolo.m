1909f95e446a8c9d1b01793c5f9ec301cc0bd1d8
giovanni-di-donato <84123970+giovanni-di-donato@users.noreply.github.com>
2021-06-11 16:36:32 +0200

    completato studio R0, condiioni iniziali

--- R0_calcolo.m
+++ R0_calcolo.m
@@ -108,7 +108,7 @@ t_radd1 = t(selez1)
 %l'idea è metetre delle persone in tutti i compartimenti e vedere quando
 %raddoppiano
 
-x0 = [1-E0, E0*ones(1,8) zeros(1,15) ];
+x0 = [1-8*E0, E0*ones(1,8) zeros(1,15) ];
 
 %cercando il tasso di raddoppio seleziono tradd in t quando ho
 %tra gli espostiv E quando raggiungo 2*E0
@@ -264,6 +264,109 @@ legend('Malati totali (E,P,I,A,H,Q)')
 
 selez1 = (malati< (2*E0 + 5e-8) & malati > (2*E0 - 5e-8));
 t_radd_tot = t(selez1)
+%
+%% ora cerchiamo di capire che succede se cambio le condizioni iniziali
+
+%l'idea è metetre delle persone in tutti i compartimenti e vedere quando
+%raddoppiano
+
+x0_casc = [1-8*E0, 0, E0, E0, E0 E0  0 0 E0 E0 E0 E0 zeros(1,19) ];
+
+%cercando il tasso di raddoppio seleziono tradd in t quando ho
+%tra gli espostiv E quando raggiungo 2*E0
+tic
+[t,x_vaccini_tot]= ode45('gatto_vaccini_unico_cascate', 0:0.02:7, x0_casc,options); 
+toc
+
+
+figure(1)
+E = x_vaccini_tot(:,3);
+plot(t, E);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Esposti')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (E< (2*E0 + 1e-8) & E > (2*E0 - 1e-8));
+t_radd_E = t(selez)
+
+%e così via vediamo le altre variabili
+
+figure(2)
+P = x_vaccini_tot(:,4);
+plot(t, P);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Paucisintomatici')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (P< (2*E0 + 1e-8) & P > (2*E0 - 1e-8));
+t_radd_P = t(selez)
+
+%e così via per le altre variabili
+
+figure(3)
+I = x_vaccini_tot(:,5);
+plot(t, I);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Infetti')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (I< (2*E0 + 1e-8) & I > (2*E0 - 1e-8));
+t_radd_I = t(selez)
+
+%anche se sembra che per raddoppiare ci voglia sempre più tempo
+
+figure(4)
+A = x_vaccini_tot(:,6);
+plot(t, A);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Asintomatici')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (A< (2*E0 + 1e-8) & A > (2*E0 - 1e-8));
+t_radd_A = t(selez)
+
+%quindi vediamo la somma di tutti i componenti
+
+trasmettitori = P+I+A ;% domanda: ha senso aggiungere anche chi non trasmette?E + x_vaccini_tot(:, 6) + x_vaccini_tot(:, 7);
+figure(5)
+plot(t, trasmettitori);
+hold on
+plot(t, 2*E0*3*ones(length(t)));
+hold off
+legend('Trasmettitori (P,I,A)')
+
+selez = (trasmettitori< (2*3*E0 + 3e-8) & trasmettitori> (2*3*E0 - 3e-8));
+t_radd_trasm = t(selez)
+
+figure(6)
+malati = E + P + I + A + x_vaccini_tot(:, 9) + x_vaccini_tot(:, 10);
+plot(t, malati);
+hold on
+plot(t, 2*E0*6*ones(length(t)));
+hold off
+legend('Malati totali (E,P,I,A,H,Q)')
+
+selez1 = (malati< (2*6*E0 + 5e-8) & malati > (2*6*E0 - 5e-8));
+t_radd_tot = t(selez1)
+
+
+
 %% rifacciamo l'analisi del tasso di raddoppio nel nuovo modello per vedere se c'è stato un rallentamento 
 %causa cambio ipotes i sulle distribuzioni degli infetti (da esponenziale a gamma) 
 global x0_casc_inf
@@ -317,4 +420,108 @@ hold off
 legend('Trasmettitori')
 
 selez1 = (trasmettitori< (2*E0 + 5e-8) & trasmettitori > (2*E0 - 5e-8));
+t_radd_tot = t(selez1)
+
+%
+
+
+
+%% ora cerchiamo di capire che succede se cambio le condizioni iniziali
+
+%l'idea è metetre delle persone in tutti i compartimenti e vedere quando
+%raddoppiano
+
+x0_casc_inf = [1-7*E0, E0, E0, 0 0 E0 E0 E0 E0 E0 zeros(1,20) ];
+
+%cercando il tasso di raddoppio seleziono tradd in t quando ho
+%tra gli espostiv E quando raggiungo 2*E0
+tic
+[t,x_vaccini_tot]= ode45('gatto_vaccini_unico_cascatesoloInfetti', 0:0.02:7, x0_casc_inf,options); 
+toc
+
+
+figure(1)
+E = x_vaccini_tot(:,2);
+plot(t, E);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Esposti')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (E< (2*E0 + 1e-8) & E > (2*E0 - 1e-8));
+t_radd_E = t(selez)
+
+%e così via vediamo le altre variabili
+
+figure(2)
+P = x_vaccini_tot(:,3);
+plot(t, P);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Paucisintomatici')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (P< (2*E0 + 1e-8) & P > (2*E0 - 1e-8));
+t_radd_P = t(selez)
+
+%e così via per le altre variabili
+
+figure(3)
+I = x_vaccini_tot(:,6);
+plot(t, I);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Infetti')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (I< (2*E0 + 1e-8) & I > (2*E0 - 1e-8));
+t_radd_I = t(selez)
+
+%anche se sembra che per raddoppiare ci voglia sempre più tempo
+
+figure(4)
+A = x_vaccini_tot(:,7);
+plot(t, A);
+hold on
+plot(t, 2*E0*ones(length(t)));
+hold off
+legend('Asintomatici')
+
+%calcolo tasso di raddoppio t_radd tramite selezioni dei valori di t, con
+%il vettore logico di selezione selez
+
+selez = (A< (2*E0 + 1e-8) & A > (2*E0 - 1e-8));
+t_radd_A = t(selez)
+
+%quindi vediamo la somma di tutti i componenti
+
+trasmettitori = P+I+A ;% domanda: ha senso aggiungere anche chi non trasmette?E + x_vaccini_tot(:, 6) + x_vaccini_tot(:, 7);
+figure(5)
+plot(t, trasmettitori);
+hold on
+plot(t, 2*E0*3*ones(length(t)));
+hold off
+legend('Trasmettitori (P,I,A)')
+
+selez = (trasmettitori< (2*3*E0 + 3e-8) & trasmettitori> (2*3*E0 - 3e-8));
+t_radd_trasm = t(selez)
+
+figure(6)
+malati = E + P + I + A + x_vaccini_tot(:, 9) + x_vaccini_tot(:, 8);
+plot(t, malati);
+hold on
+plot(t, 2*E0*6*ones(length(t)));
+hold off
+legend('Malati totali (E,P,I,A,H,Q)')
+
+selez1 = (malati< (2*6*E0 + 5e-8) & malati > (2*6*E0 - 5e-8));
 t_radd_tot = t(selez1)
