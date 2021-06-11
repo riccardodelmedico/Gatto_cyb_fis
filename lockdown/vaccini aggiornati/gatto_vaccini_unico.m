972dd5da63383020b1294e47aafc26b9e7923991
atempestapriv <84441645+atempestapriv@users.noreply.github.com>
2021-06-11 10:34:22 +0200

    Add files via upload

---
+++ lockdown/vaccini aggiornati/gatto_vaccini_unico.m
@@ -0,0 +1,75 @@
+function xdot= gatto_vaccini_unico(t,x)
+
+global lambda deltaE deltaP sig eta gammaI alfaI gammaA zeta gammaH alfaH ...
+    gammaQ betaP betaA betaI N x0 eff1 eff2 ef1 prima_d seconda_d ...
+    Lvect teta
+
+S = x(1); %suscettibili
+E = x(2); %esposti
+P = x(3); %presintomatici
+I = x(4); %infetti
+A = x(5); %asintomatici
+H = x(6); %ospedalizzati
+Q = x(7); %quarantena casalinga
+R = x(8); %recuperati
+D = x(9); %dead
+% parameters_vaccini;
+% dati_vaccini;
+S1 = x(10); %suscettibili
+E1 = x(11); %esposti
+P1 = x(12); %presintomatici
+I1 = x(13); %infetti
+A1 = x(14); %asintomatici
+H1 = x(15); %ospedalizzati
+Q1 = x(16); %quarantena casalinga
+R1 = x(17); %recuperati
+D1 = x(18); %dead
+% parameters_vaccini;
+% dati_vaccini;
+S2 = x(19); %suscettibili
+E2 = x(20); %esposti gruppo
+P2 = x(21); %presintomatici
+I2 = x(22); %infetti
+A2 = x(23); %asintomatici
+R2 = x(24); %recuperati
+
+prima_dos=prima_d(fix(t)+1);
+seconda_dos=seconda_d(fix(t)+1);
+L=Lvect(fix(t)+1);
+
+
+lambda= (betaP*(P+P1+P2) + betaI*(I+I1+I2) + betaA*(A+A1+A2))/(S + E + (I+I1+I2) + (A+A1+A2) + R);
+xdot = zeros(24,1);
+
+    xdot(1) = - lambda*S*(1-teta*L)^2-eff1*prima_dos;
+    xdot(2) = lambda*S*(1-teta*L)^2 - deltaE*E;
+    xdot(3) = deltaE*E - deltaP*P;
+    xdot(4) = sig*deltaP*P - (eta + gammaI + alfaI)*I;
+    xdot(5) = (1 - sig)*deltaP*P - gammaA*A;
+    xdot(6) = (1 - zeta)*eta*I - (gammaH + alfaH)*H;
+    xdot(7) = zeta*eta*I - gammaQ*Q;
+    xdot(8) = gammaI*I + gammaA*A + gammaH*H;
+    xdot(9) = alfaI*I + alfaH*H;
+    %%%%POI LA SECONDA DINAMICA, CHE è ACCOPPIATA TRAMITE LAMBDA
+    
+    xdot(10) = - lambda*S1*(1-teta*L)^2+eff1*prima_dos-eff2*seconda_dos;
+    xdot(11) = lambda*S1*(1-teta*L)^2 - deltaE*E1;
+    xdot(12) = deltaE*E1 - deltaP*P1;
+    xdot(13) = sig*deltaP*P1 - ((1-ef1)*eta + gammaI +(1-ef1)*alfaI)*I1;
+    xdot(14) = (1 - sig)*deltaP*P1 - gammaA*A1;
+    xdot(15) = (1 - zeta)*(1-ef1)*eta*I1 - (gammaH +(1-ef1)*alfaH)*H1;
+    xdot(16) = zeta*(1-ef1)*eta*I1 - gammaQ*Q1;
+    xdot(17) = gammaI*I1 + gammaA*A1 + gammaH*H1;
+    xdot(18) = (1-ef1)*alfaI*I1 + (1-ef1)*alfaH*H1;
+    
+    %%%%%E SIMILMENTE LA TERZA
+    xdot(19) = - lambda*S2*(1-teta*L)^2+eff2*seconda_dos;
+    xdot(20) = lambda*S2*(1-teta*L)^2 - deltaE*E2;
+    xdot(21) = deltaE*E2 - deltaP*P2;
+    xdot(22) = sig*deltaP*P2 - gammaA*I2;
+    xdot(23) = (1 - sig)*deltaP*P2 - gammaA*A2;
+    xdot(24) = gammaA*I2 + gammaA*A2;
+
+
+end
+
---
+++ lockdown/vaccini aggiornati/parameters_vaccini.m
@@ -0,0 +1,45 @@
+global lambda deltaE deltaP sig eta gammaI alfaI gammaA zeta gammaH ...
+    alfaH gammaQ gammaA betaP betaI betaA eff1 eff2 ef1 teta
+
+R0=3.6;
+deltaE= 1 / 3.32;
+deltaP= 1 / 0.75;
+eta= 1 / 4.05;
+gammaI= 1 / 14.32;
+gammaQ=gammaI;
+gammaH= gammaI;
+gammaA= 2*gammaI;
+alfaI= 1 / 24.23;
+sig= 0.25; % (1-sigma) frazione degli asintomatici
+%betaP = R0/(1/deltaP + 1.03*sig/( eta + alfaI + gammaI)+ 0.033*(1-sig) / gammaA);
+betaA_betaP= 0.033; %betaA asymptomatic transmission rate
+betaI_betaA= 1.03;
+% betaP1/ betaP== 0.82; % betaP1 after restriction on february 22, 2020
+% betaP2/ betaP1== 0.66; % betaP2 after restriction on march 8, 2020
+% deltat0= 34.94;
+% omega= 7.84;
+zeta= 0.4; % fraction of sympomatic infected being quarantined
+alfaH = alfaI;
+% rs= 0.5;
+eff1 = 0.9;
+eff2 = 1;
+ef1 = 0.7;
+teta = 1; %efficacia del lockdown
+% options = optimoptions('fmincon','Display','none','Algorithm','active-set',...
+%     'OptimalityTolerance', 1e-1, 'MaxFunctionEvaluations', 5,'FunValCheck','on');
+
+x1=1/deltaP;
+x2=sig/(eta+alfaI+gammaI);
+x3=(1-sig)/gammaA;
+
+B = [0 0 3.6].'; %termini noti
+X = [1 0 -betaA_betaP; -betaI_betaA 1 0; x3 x2 x1];
+A = linsolve(X,B);
+betaA = A(1);
+betaI = A(2);
+betaP = A(3);
+nolockdown= 25;
+novax= 270;
+Mvax = size(prima_dose_norm);
+NV = Mvax(1);
+N= nolockdown + novax + NV;
